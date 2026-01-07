78ab56ed2b71d76ff1596e3c42390d84823a3f08
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-07 16:20:23 -0300

    added initial routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,14 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SignUp from '../pages/Signup';
 import SignIn from '../pages/SignIn';  
 
 function RoutesApp() {
     return(
-        <BrowserRouter>
         <Routes>
-            <Route path='' element={ <SignIn /> } />
-            <Route path='' element={ <SignUp /> } />
-
-
-        </Routes>
-        </BrowserRouter>
+            <Route path='/' element={ <SignIn /> } />
+            <Route path='/register' element={ <SignUp /> } />
+        </Routes>       
     )
 }
 
