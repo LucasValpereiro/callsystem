faf108e3d8c67e860ae8b2b4796e5793e6881326
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-13 20:43:08 -0300

    added dashboard route

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import {} from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Página de Dashboard</h1>
+        </div>
+    )
+}
