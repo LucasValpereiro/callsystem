12b51219571ccffc11ad7a17a88f052d860df33e
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-14 12:55:11 -0300

    added photo upload

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -16,6 +16,23 @@ export default function Profile(){
 
 
     const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+    const [imageAvatar, setImageAvatar] = useState(null)
+
+    function handleFile(e){
+        if(e.target.files[0]){
+            const image = e.target.files[0];
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png'){
+                setImageAvatar(image)
+                setAvatarUrl(URL.createObjectURL(image))
+            } else{
+                alert('Envie uma imagem do tipo PNG ou JPEG')
+                setImageAvatar(null);
+                return;
+            }
+
+        }
+    }
 
     return(
         <div>
@@ -36,7 +53,7 @@ export default function Profile(){
                 <FiUpload color='#FFF' size={25} />
              </span>
 
-            <input type='file' accept='image/*'/> <br/>
+            <input type='file' accept='image/*' onChange={handleFile}/> <br/>
             {avatarUrl === null ? (
                 <img src={avatar} alt='Foto de Perfil' width={250} height={250}/>
             ) : (
