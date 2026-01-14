ffd44ec286f07bc607b4d793ef8187aa38bb967a
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-14 11:54:55 -0300

    added avatar upload input

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -8,3 +8,4 @@ export default function Title({ children, name }){
         </div>
     )
 }
+
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,17 @@
+import { useContext, useState } from 'react'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 
-import { FiSettings } from 'react-icons/fi'
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../contexts/auth'
 
 export default function Profile(){
+
+    const { user } = useContext(AuthContext);
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
     return(
         <div>
             <Header/>
@@ -12,9 +20,32 @@ export default function Profile(){
                <Title name='Minha conta'>
                 <FiSettings size={25} />
                </Title>
+
+            <div className='container'>
+
+            <form className='form-profile'>
+
+            <label className='label-avatar'>
+
+             <label>
+                <FiUpload color='#FFF' size={25} />
+             </label>
+
+            <input type='file' accept='image/*'/> <br/>
+            {avatarUrl === null ? (
+                <img src={avatar} alt='Foto de Perfil' width={250} height={250}/>
+            ) : (
+                <img src={avatarUrl} alt='Foto de Perfil' width={250} height={250}/>
+            )}
+
+            </label>
+
+            </form>
+
             </div>
 
-            <h1>Página Perfil</h1>
+            </div>
+           
         </div>
     )
 }
