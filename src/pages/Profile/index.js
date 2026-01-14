d2cd157ce9fb5670b8b1fb46332bd1ebd6ca94fa
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-14 15:24:34 -0300

    att upload imgs

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,6 +6,12 @@ import { FiSettings, FiUpload } from 'react-icons/fi'
 import avatar from '../../assets/avatar.png';
 import { AuthContext } from '../../contexts/auth'
 
+import { db, storage } from '../../services/firebaseConnection'
+import { doc, updateDoc } from 'firebase/firestore'
+import { ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+
+import { toast } from 'react-toastify'
+
 import './profile.css';
 
 export default function Profile(){
@@ -34,6 +40,64 @@ export default function Profile(){
         }
     }
 
+    async function handleUpload(){
+        const currentUid = user.uid;
+
+        const uploadRef = ref(storage, `images/${currentUid}/${imageAvatar.name}`);
+
+        const uploadTask = uploadBytes(uploadRef, imageAvatar)
+        .then((snapshot) => {
+            
+            getDownloadURL(snapshot.ref).then( async (downloadURL) => {
+                let urlFoto = downloadURL;
+
+                const docRef = doc(db, 'users', user.uid)
+                await updateDoc(docRef, {
+                    avatarUrl: urlFoto,
+                    nome: nome,
+                })
+                .then( () => {
+                    let data = {
+                    ...user,
+                    nome: nome,
+                    avatarUrl: urlFoto,
+                }
+
+                setUser(data);
+                storageUser(data);
+                toast.success('Atualizado com sucesso!')
+                })
+            })
+
+        })
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        if(imageAvatar === null && nome !== ''){
+            // Atualizar apenas o nome do usuário
+            const docRef = doc(db, 'users', user.uid)
+            await updateDoc(docRef, {
+                nome: nome,
+            })
+            .then(() => {
+                let data = {
+                    ...user,
+                    nome: nome,
+                }
+
+                setUser(data);
+                storageUser(data);
+                toast.success('Atualizado com sucesso!')
+            })
+        } else if(nome !== '' && imageAvatar !== null){
+            // Atualizar nome e a foto
+            await handleUpload()
+        }
+
+    }
+
     return(
         <div>
             <Header/>
@@ -45,7 +109,7 @@ export default function Profile(){
 
             <div className='container'>
 
-            <form className='form-profile'>
+            <form className='form-profile' onSubmit={handleSubmit}>
 
             <label className='label-avatar'>
 
