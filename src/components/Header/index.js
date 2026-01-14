7f622ba077430fb326eb7a9f2f546a6eeff68ddb
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-14 00:06:06 -0300

    added style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../../contexts/auth';
 import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
-
+import './header.css';
 
 export default function Header(){
     const { user } = useContext(AuthContext)
