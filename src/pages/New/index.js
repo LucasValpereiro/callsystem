b5d3d4b2b1cc33e2409691592262f6f2cda7c0f6
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-19 15:24:21 -0300

    added calls registers

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -6,7 +6,9 @@ import { FiPlusCircle } from 'react-icons/fi';
 
 import { AuthContext } from '../../contexts/auth';
 import { db } from '../../services/firebaseConnection';
-import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, getDoc, doc, addDoc } from 'firebase/firestore';
+
+import { toast } from 'react-toastify'
 
 import './new.css';
 
@@ -70,6 +72,31 @@ export default function New(){
         console.log(customers[e.target.value].nomeFantasia);
     }
 
+    async function handleRegister(e){
+        e.preventDefault();
+
+        //Registrar chamado
+        await addDoc(collection(db, 'chamados'), {
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            complemento: complemento,
+            status: status,
+            userId: user.uid,
+        })
+        .then(() => {
+            toast.success('Chamado Registrado')
+            setComplemento('')
+            setCustomerSelected(0)
+        })
+
+        .catch((error) => {
+            toast.error('Ops! Erro ao registrar, tente novamente mais tarde!')
+            console.log(error);
+        })
+    }
+
     return(
         <div>
         <Header />
@@ -80,7 +107,7 @@ export default function New(){
             </Title>
 
             <div className='container'>
-                <form className='form-profile'>
+                <form className='form-profile' onSubmit={handleRegister}>
 
                 <label>Clientes</label>
                 {
