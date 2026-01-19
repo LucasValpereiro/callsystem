9afc4a9965249e683461464627647e13834befe4
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-19 13:19:22 -0300

    added structure and style to dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,7 +4,7 @@ import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiPlus, FiMessageSquare } from 'react-icons/fi';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
 
@@ -27,14 +27,38 @@ export default function Dashboard(){
             </Title>
 
            <>
-            <Link to='/new'>
+            <Link to='/new' className='new'>
             <FiPlus color='#FFF' size={25}/>
             Novo Chamado
             </Link>
 
-            <div className='container'>
-            
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>Cliente</th>
+                        <th scope='col'>Assunto</th>
+                        <th scope='col'>Status</th>
+                        <th scope='col'>Cadastrado</th>
+                        <th scope='col'>#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label='Cliente'>Mercado Esquina</td>
+                        <td data-label='Assunto'>Suporte</td>
+                        <td data-label='Status'>Em aberto</td>
+                        <td data-label='Cadastrado'>19/01/2026</td>
+                        <td data-label='#'>
+                            <button className='action' style={{ backgroundColor: '#3583f6' }}>
+                            <FiSearch  color='#FFF' size={17}/>
+                            </button>
+                            <button className='action' style={{ backgroundColor: '#f6a935' }}>
+                            <FiEdit2  color='#FFF' size={17}/>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
            </>
 
            </div>
